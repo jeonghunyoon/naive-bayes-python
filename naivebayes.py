2a45eda6b95c9f00ad4eabda9875fa7a0aec3f36
yoonjeonghun <jeonghun.yoon123@gmail.com>
2016-05-01 02:03:00 +0900

    [NAIVE-BAYES-003] training_naive_bayes method implement

--- naivebayes.py
+++ naivebayes.py
@@ -20,14 +20,34 @@ def create_voca_dictionary(dataset_list):
     voca_set = set([])
     for dataset in dataset_list:
         voca_set = voca_set | set(dataset)
-    return voca_set
+    return list(voca_set)
 
 
 def dataset_to_feat_vector(voca_dictionary, dataset):
-    feat_vector = zeros(voca_dictionary)
+    feat_vector = [0] * (len(voca_dictionary))
     for word in dataset:
         if word in voca_dictionary:
             feat_vector[voca_dictionary.index(word)] = 1
         else:
             print "the word : %s is niot in my vocabulary dictionary." % word
     return feat_vector
+
+
+def training_naive_bayes(training_matrix, training_category):
+    num_of_training_set = len(training_matrix)
+    num_of_word = len(training_matrix[0])
+    prior_c1 = sum(training_category) / float(num_of_training_set)
+    vec_of_word_count_c1 = zeros(num_of_word)  # p(w|c_1) = p(w_1|c_1)...p(w_j|c_1)
+    vec_of_word_count_c0 = zeros(num_of_word)
+    total_word_count_c1 = 0.0
+    total_word_count_c0 = 0.0
+    for i in range(num_of_training_set):
+        if training_category[i] == 1:
+            vec_of_word_count_c1 += training_matrix[i]
+            total_word_count_c1 += sum(training_matrix[i])
+        else:
+            vec_of_word_count_c0 += training_matrix[i]
+            total_word_count_c0 += sum(training_matrix[i])
+    prob_vector_c1 = vec_of_word_count_c1 / total_word_count_c1
+    prob_vector_c0 = vec_of_word_count_c0 / total_word_count_c0
+    return prob_vector_c0, prob_vector_c1, prior_c1
