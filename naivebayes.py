a4b50a8651b3ba369f83915db4cb431869c017ae
jeonghun <jeonghun.yoon123@gmail.com>
2016-05-01 15:35:15 +0900

    [NAIVE-BAYES] naive_bayes_classify, test_naive_bayes method implement

--- naivebayes.py
+++ naivebayes.py
@@ -37,10 +37,10 @@ def training_naive_bayes(training_matrix, training_category):
     num_of_training_set = len(training_matrix)
     num_of_word = len(training_matrix[0])
     prior_c1 = sum(training_category) / float(num_of_training_set)
-    vec_of_word_count_c1 = zeros(num_of_word)  # p(w|c_1) = p(w_1|c_1)...p(w_j|c_1)
-    vec_of_word_count_c0 = zeros(num_of_word)
-    total_word_count_c1 = 0.0
-    total_word_count_c0 = 0.0
+    vec_of_word_count_c1 = ones(num_of_word)  # p(w|c_1) = p(w_1|c_1)...p(w_j|c_1)
+    vec_of_word_count_c0 = ones(num_of_word)
+    total_word_count_c1 = 2.0
+    total_word_count_c0 = 2.0
     for i in range(num_of_training_set):
         if training_category[i] == 1:
             vec_of_word_count_c1 += training_matrix[i]
@@ -51,3 +51,29 @@ def training_naive_bayes(training_matrix, training_category):
     prob_vector_c1 = vec_of_word_count_c1 / total_word_count_c1
     prob_vector_c0 = vec_of_word_count_c0 / total_word_count_c0
     return prob_vector_c0, prob_vector_c1, prior_c1
+
+
+def naive_bayes_classify(vec_to_classify, prob_vector_c1, prob_vector_c0, prior_c1):
+    log_prob_vector_c1 = log(prob_vector_c1)
+    log_prob_vector_c0 = log(prob_vector_c0)
+    log_posterior_c1 = sum(log_prob_vector_c1 * vec_to_classify) + log(prior_c1) # log(p(c_1|w)) = log(p(w|c_1)p(c_1))
+    log_posterior_c0 = sum(log_prob_vector_c0 * vec_to_classify) + log(1.0 - prior_c1) # log(p(c_0|w)) = log(p(w|c_0)p(c_0))
+    if log_posterior_c1 > log_posterior_c0:
+        return 1
+    else:
+        return 0
+
+
+def test_naive_bayes():
+    dataset_list, class_vector = load_datasets()
+    voca_dictionary = create_voca_dictionary(dataset_list)
+    training_mat = []
+    for dataset in dataset_list:
+        training_mat.append(dataset_to_feat_vector(voca_dictionary, dataset))
+    prob_vector_c0, prob_vector_c1, prior_c1 = training_naive_bayes(training_mat, class_vector)
+    test_vector_1 = ['love', 'my', 'dalmation']    # test 1
+    test_feat_vec_1 = dataset_to_feat_vector(voca_dictionary, test_vector_1)
+    print test_vector_1, 'classified as : ', naive_bayes_classify(test_feat_vec_1, prob_vector_c1, prob_vector_c0, prior_c1)
+    test_vector_2 = ['stupid', 'garbage']    # test 2
+    test_feat_vec_2 = dataset_to_feat_vector(voca_dictionary, test_vector_2)
+    print test_vector_2, 'classified as : ', naive_bayes_classify(test_feat_vec_2, prob_vector_c1, prob_vector_c0, prior_c1)
