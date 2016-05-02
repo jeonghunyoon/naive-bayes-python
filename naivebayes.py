8fb4da0e4ea16432a39bb6889a8ca4064bacfcb2
davidyoon <davidyoon@coupang.com>
2016-05-02 09:10:23 +0900

    [NAIVE-BAYES-006] verify_classifier method implementation

--- naivebayes.py
+++ naivebayes.py
@@ -57,8 +57,7 @@ def naive_bayes_classify(vec_to_classify, prob_vector_c1, prob_vector_c0, prior_
     log_prob_vector_c1 = log(prob_vector_c1)
     log_prob_vector_c0 = log(prob_vector_c0)
     log_posterior_c1 = sum(log_prob_vector_c1 * vec_to_classify) + log(prior_c1)  # log(p(c_1|w)) = log(p(w|c_1)p(c_1))
-    log_posterior_c0 = sum(log_prob_vector_c0 * vec_to_classify) + log(
-        1.0 - prior_c1)  # log(p(c_0|w)) = log(p(w|c_0)p(c_0))
+    log_posterior_c0 = sum(log_prob_vector_c0 * vec_to_classify) + log(1.0 - prior_c1)  # log(p(c_0|w)) = log(p(w|c_0)p(c_0))
     if log_posterior_c1 > log_posterior_c0:
         return 1
     else:
@@ -74,12 +73,10 @@ def test_naive_bayes():
     prob_vector_c0, prob_vector_c1, prior_c1 = training_naive_bayes(training_mat, class_vector)
     test_vector_1 = ['love', 'my', 'dalmation']  # test 1
     test_feat_vec_1 = dataset_to_feat_vector(voca_dictionary, test_vector_1)
-    print test_vector_1, 'classified as : ', naive_bayes_classify(test_feat_vec_1, prob_vector_c1, prob_vector_c0,
-                                                                  prior_c1)
+    print test_vector_1, 'classified as : ', naive_bayes_classify(test_feat_vec_1, prob_vector_c1, prob_vector_c0, prior_c1)
     test_vector_2 = ['stupid', 'garbage']  # test 2
     test_feat_vec_2 = dataset_to_feat_vector(voca_dictionary, test_vector_2)
-    print test_vector_2, 'classified as : ', naive_bayes_classify(test_feat_vec_2, prob_vector_c1, prob_vector_c0,
-                                                                  prior_c1)
+    print test_vector_2, 'classified as : ', naive_bayes_classify(test_feat_vec_2, prob_vector_c1, prob_vector_c0, prior_c1)
 
 
 def text_parser(text):
